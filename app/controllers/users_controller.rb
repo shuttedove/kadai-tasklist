6355701a107997829b2ee608747fd6c0149bbc60
shuttedove <shunichiro.tago@gmail.com>
2017-09-02 09:22:32 +0000

    login revised2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @task = current_user.tasks   
-  endq
+  end
 
   def new
       @user = User.new
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
